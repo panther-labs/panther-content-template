abf4dbe24237d8d81be602b8e58bb920bc1a60aa
maxrichie5 <maxrichie5@users.noreply.github.com>
2022-11-04 22:01:00 +0000

    automatic formatting

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,6 +4,7 @@ import unittest
 from panther_content.__main__ import always_true, always_true_detection
 from panther_sdk import detection
 
+
 class TestMain(unittest.TestCase):
     def test_okta_api_key_created(self) -> None:
         self.assertIsInstance(always_true_detection, detection.Rule)
